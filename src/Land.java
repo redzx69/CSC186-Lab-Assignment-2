c2ff80b6781b35fc646c0e39d49d8859d3c21ad7
redzx69 <115964926+redzx69@users.noreply.github.com>
2023-04-14 16:02:43 +0800

    update

---
+++ src/Land.java
@@ -0,0 +1,110 @@
+public class Land {
+    private String id;
+    private String ownerName;
+    private String houseType;
+    private double area;
+
+    //default constructor
+    public Land() 
+    {
+        id = "";
+        ownerName = "";
+        houseType = "";
+        area = 0;
+    }
+
+    //normal constructor
+    public Land(String id, String ownerName, String houseType, double area)
+    {
+        this.id = id;
+        this.ownerName = ownerName;
+        this.houseType = houseType;
+        this.area = area;
+    }
+
+    //copy constructor
+    public Land(Land copyLand) 
+    {
+        this.id = copyLand.id;
+        this.ownerName = copyLand.ownerName;
+        this.houseType = copyLand.houseType;
+        this.area = copyLand.area;
+    }
+
+    //setter
+    public void setId()
+    {
+        this.id = id;
+    }
+
+    public void setOwnerName()
+    {
+        this.ownerName = ownerName;
+    }
+
+    public void setHouseType()
+    {
+        this.houseType = houseType;
+    }
+
+    public void setArea()
+    {
+        this.area = area;
+    }
+
+    //getter
+    public String getId()
+    {
+        return this.houseType;
+    }
+    
+    public String getOwnerName()
+    {
+        return this.ownerName;
+    }
+
+    public String getHouseType()
+    {
+        return this.houseType;
+    }
+
+    public double getArea()
+    {
+        return this.area;
+    }
+
+    //processor
+    public double calcTaxPrice()
+    {
+        double taxRate = 0, taxPrice = 0; // RM-per-m^2
+        switch (getHouseType().charAt(0)) {
+            case 't':
+            case 'T':
+                taxRate = 10;           
+                break;
+            case 's':
+            case 'S':
+                taxRate = 15;           
+                break;
+            case 'b':
+            case 'B':
+                taxRate = 20;           
+                break;
+            case 'c':
+            case 'C':
+                taxRate = 30;           
+                break;
+            default:
+                System.out.println("Invalid house type.");
+                break;
+        }
+        taxPrice = taxRate*getArea();
+        return taxPrice;
+    }
+
+    //printer
+    public String toString()
+    {
+        return String.format("%nLand ID:\t%s%nOwner Name:\t%s%nHouse Type:\t%s%nArea of land:\t%.2f m^2%nTax Price:\tRM%.2f%n", id, ownerName, houseType, area, calcTaxPrice());
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -4,36 +4,94 @@ public class Main {
     
     public static void main(String[] args) 
     {
-        //instantiate an object
-        Program p = new Program();
-        
-        //input data
+        int choice = selectProgram();
+        if(choice == 1 )
+        {
+            //instantiate an object
+            Program p = new Program();
+            
+            //input data
+            Scanner in = new Scanner(System.in);
+            System.out.print("\nEnter program code: ");
+            String pc = in.nextLine();
+            System.out.print("Enter program description: ");
+            String pd = in.nextLine();
+            System.out.print("Enter duration: ");
+            String dr = in.nextLine();
+            System.out.print("Enter faculty: ");
+            String fc = in.nextLine();
+            System.out.print("Enter program head: ");
+            String ph = in.nextLine();
+            in.close();
+
+            //store onto object
+            //normal constructor
+            p = new Program(pc, pd, dr, fc, ph);
+
+            //OR
+            //Setter
+            p.setProgCode(pc);
+            p.setProgDesc(pd);
+            p.setDuration(dr);
+            p.setFaculty(fc);
+            p.setProgHead(ph);
+
+            //manipulate data
+            System.out.println(p.toString());
+        }
+        else if(choice == 2)
+        {
+            Land l = new Land();
+
+            Scanner in = new Scanner(System.in);
+            System.out.print("\nEnter land id: ");
+            String id = in.nextLine();
+            System.out.print("Enter owner name: ");
+            String ownerName = in.nextLine();
+            System.out.print("Enter house type [T,S,B,C]: ");
+            String houseType = in.nextLine();
+                switch (houseType.charAt(0)) {
+                    case 't':
+                    case 'T':
+                        houseType = "Terrace";           
+                        break;
+                    case 's':
+                    case 'S':
+                        houseType = "Semi-Detached";           
+                        break;
+                    case 'b':
+                    case 'B':
+                        houseType = "Bungalow";           
+                        break;
+                    case 'c':
+                    case 'C':
+                        houseType = "Condominium";           
+                        break;
+                    default:
+                        System.out.println("Invalid house type.");
+                        System.exit(1);
+                        break;
+                }
+            System.out.print("Enter area: ");
+            double area = in.nextDouble();
+            in.close();
+
+            l = new Land(id, ownerName, houseType, area);
+
+            System.out.println(l.toString());
+        }
+    }
+
+    public static int selectProgram()
+    {
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter program code: ");
-        String pc = in.nextLine();
-        System.out.print("Enter program description: ");
-        String pd = in.nextLine();
-        System.out.print("Enter duration: ");
-        String dr = in.nextLine();
-        System.out.print("Enter faculty: ");
-        String fc = in.nextLine();
-        System.out.print("Enter program head: ");
-        String ph = in.nextLine();
-        in.close();
-
-        //store onto object
-        //normal constructor
-        p = new Program(pc, pd, dr, fc, ph);
-
-        //OR
-        //Setter
-        p.setProgCode(pc);
-        p.setProgDesc(pd);
-        p.setDuration(dr);
-        p.setFaculty(fc);
-        p.setProgHead(ph);
-
-        //manipulate data
-        System.out.println(p.toString());
+        System.out.printf("%n1.University Program Level%n2.Land details%n%nSelect program[1-2]: ");
+        int choice = in.nextInt();
+            if(choice != 1 && choice != 2)
+            {
+                System.out.println("Invalid choice.");
+                System.exit(1);
+            }
+        return choice;
     }
 }
